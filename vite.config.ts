cffedd2586b3589fbb5f4788d2026ef063984895
arash-vaziri <arash.h.vaziri@gmail.com>
2025-06-11 12:27:12 +0800

    - init with ynpx and start with chakra

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vite.dev/config/
 export default defineConfig({
-
-    server: {
+  plugins: [ react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    }
+  },
+  server: {
       host: '0.0.0.0',
       port: 5173,
       strictPort: true
-    }
+  }
 })
