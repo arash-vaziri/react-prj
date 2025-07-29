782342abaf60f42f6e3efba52daebdcfbfd770d7
arash-vaziri <arash.h.vaziri@gmail.com>
2025-07-28 23:04:09 -0400

    -responsive layout - chakra 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import "./App.css";
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show, useBreakpointValue } from "@chakra-ui/react";
 
 function App() {
+  const aboveLg = useBreakpointValue({
+    base: false,
+    md: false,
+    lg: true,
+  });
+
   return (
     <>
-      <Button colorPalette={"blue"}> Click Me </Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <Show when={aboveLg}>
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="blue">
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
