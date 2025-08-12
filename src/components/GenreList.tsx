879f293d0d4cf8675b9b6761c59678da1727a5ea
arash-vaziri <arash.h.vaziri@gmail.com>
2025-08-11 20:09:00 -0400

    - cleanup genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,12 @@
-import { List, Text, HStack, Image, Spinner, Button } from "@chakra-ui/react";
+import {
+  List,
+  Text,
+  HStack,
+  Image,
+  Spinner,
+  Button,
+  Heading,
+} from "@chakra-ui/react";
 import useGenre, { type Genre } from "@/hooks/useGenre";
 import cropImageUrl from "@/services/cropImageUrl";
 
@@ -15,6 +23,9 @@ const GenreList = ({ onGenreSelected, selectedGenre }: Props) => {
   return (
     <>
       {error && <Text> {error} </Text>}
+      <Heading as="h2" marginY={3} fontSize="1.5rem">
+        Genres
+      </Heading>
       <List.Root variant="plain">
         {data.map((genre) => (
           <List.Item paddingY="0.5em" key={genre.id}>
@@ -23,12 +34,14 @@ const GenreList = ({ onGenreSelected, selectedGenre }: Props) => {
                 src={cropImageUrl(genre.image_background)}
                 boxSize="2em"
                 borderRadius="0.3em"
+                objectFit="cover"
               />
               <Button
                 value={genre.id}
                 variant="plain"
                 fontWeight={genre.id == selectedGenre?.id ? "bold" : "normal"}
                 onClick={() => onGenreSelected(genre)}
+                whiteSpace="normal"
               >
                 {genre.name}
               </Button>
